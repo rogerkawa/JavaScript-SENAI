df6c4db9353ceb1a24120635763cfda00c1365d5
Roger Kawã <rogerkawa8@gmail.com>
2026-04-14 21:09:29 -0300

    Add files via upload

--- for/script1.js
+++ for/script1.js
@@ -4,21 +4,27 @@ let qtd
 let total = 0
 let totalGeral = 0
 let acumuladora = 0
+let maiorPreco = 0
 let produtos = +prompt('Qual a quantidade de produtos comprados: ')
 
 for(let c = 1; c <= produtos; c++){
     alert(`Produto ${c}`)
     nome = prompt('Nome do produto: ')
     preco = +prompt('Preço do produto: ')
-    qtd = +prompt('Quantidade: ')
+    qtd = +prompt(`Quantidade de ${nome}:`)
     total = preco * qtd
     totalGeral += total
     if(preco>100){
         acumuladora += 1
     }
+    if (maiorPreco < preco){
+        maiorPreco = preco
+    }
     
 
 }
-alert(`Valor total da compra: ${totalGeral} reais
+alert(`
+    Valor total da compra: ${totalGeral} reais
     Produtos mais caros que R$100: ${acumuladora} Produtos
-    Produto mais caro: ${Math.max(preco)}`)
+    Produto mais caro: ${maiorPreco}
+    `)
