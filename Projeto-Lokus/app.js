77e0e0a1831d7b33b3d14edf4e2f6cb51380759e
Roger Kawã <rogerkawa8@gmail.com>
2026-05-05 20:04:56 -0300

    Add files via upload

--- Projeto-Lokus/app.js
+++ Projeto-Lokus/app.js
@@ -1,4 +1,42 @@
-let title = document.querySelector('.app__title')
+const banner = document.querySelector('.app__image')
+const titulos = document.querySelector('.app__title')
+const html = document.documentElement /* Trazendo elemento html fora do body */
+const botoes = document.querySelectorAll('.app__card-button[data-contexto]')
+
+botoes.forEach(itemBotoes => {
+    itemBotoes.addEventListener('click', ()=>{
+        let contexto = itemBotoes.dataset.contexto //pega o valor do data-contexto do botão
+        alterarConteudos(contexto, itemBotoes) // Chamando a função das ações
+    })
+})
+
+// Função que vai executar as ações do evento de click nos botões
+function alterarConteudos(contexto, botaoClicado){
+
+    // Removendo classe ativa dos elementos
+    botoes.forEach(itemBotao => itemBotao.classList.remove('active'))
+
+    // Adicionando a classe ativa nos elementos
+    botaoClicado.classList.add('active')
+
+    // Atualizando o data-contexto
+    html.dataset.contexto = contexto  //recebe o valor do data-contexto atual e altera a pagina
+
+    if(contexto == 'descanso-curto'){
+        titulos.innerHTML = "Foque no que gera resultado, <strong style='font-weight: bold;'>deixe o resto em segundo plano.</strong>"
+        banner.src = 'imagens/descanso-curto.png' //altera o caminho da imagem
+    }else if(contexto == 'descanso-longo'){
+        titulos.innerHTML = "Elimine distrações, <strong style='font-weight: bold;'>concentre sua energia no essencial.</strong>"
+        banner.src = 'imagens/descanso-longo.png'
+    }else if(contexto == 'foco'){
+        titulos.innerHTML = "Otimize sua produtividade, <strong style='font-weight: bold;'> mergulhe no que importa.</strong>"
+        banner.src = 'imagens/foco.png'
+    }
+    
+}
+
+
+/* let title = document.querySelector('.app__title')
 let body = document.querySelector('body')
 let image = document.querySelector('.app__image')
 
@@ -15,7 +53,6 @@ function descansoCurto(){
     foco.classList.remove('active')
     dLongo.classList.remove('active')
 }
-
 
 //foco
 let foco = document.querySelector('.active')
@@ -40,4 +77,4 @@ function descansoLongo(){
     dCurto.classList.remove('active')
     foco.classList.remove('active')
     dLongo.classList.add('active')
-}
+} */
