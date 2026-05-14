a453e55f41d9eff7363bc44286b88ec29cffed8b
Roger Kawã <rogerkawa8@gmail.com>
2026-05-13 22:43:17 -0300

    Add files via upload

--- EscalaçãoTimeDOM/app.js
+++ EscalaçãoTimeDOM/app.js
@@ -1,32 +1,77 @@
+
 let jogadores = []
 
 const body = document.querySelector('body')
-const position = document.createElement('input')
+
 const nome = document.createElement('input')
+const position = document.createElement('input')
 const number = document.createElement('input')
-const botaoAdd = document.getElementById('enviar')
 
-nome.setAttribute('placeholder', 'Digite o nome do jogador')
-number.setAttribute('placeholder', 'Digite o numero do jogador')
-position.setAttribute('placeholder', 'Digite a posição do jogador')
-const div = document.createElement('div')
+const btnAdd = document.createElement('button')
+const btnRemove = document.createElement('button')
 
-//botao de remover
-const remove = document.createElement('button')
-//botao de enviar
-const add = document.createElement('button')
-add.innerText = 
-remove.innerText = 'Remover'
-botaoAdd.addEventListener('click',()=>{
-    body.append(nome, position, number, remove)
+const lista = document.createElement('div')
+
+nome.classList.add('input')
+position.classList.add('input')
+number.classList.add('input')
+
+nome.placeholder = 'Digite o nome do jogador'
+position.placeholder = 'Digite a posição do jogador'
+number.placeholder = 'Digite o número do jogador'
+
+btnAdd.innerText = 'Adicionar Jogador'
+btnRemove.innerText = 'Remover Jogador'
+
+body.append(nome, position, number, btnAdd, btnRemove, lista)
+
+btnAdd.addEventListener('click', () => {
+
+    const escalado = {
+        nome: nome.value,
+        posicao: position.value,
+        numero: number.value
+    }
+
+    jogadores.push(escalado)
+
+    renderizarJogadores()
+
+    nome.value = ''
+    position.value = ''
+    number.value = ''
 })
 
-let escalado = {
-    nome: nome.value,
-    posicao: position.value,
-    numero: number.value
+
+function renderizarJogadores() {
+
+    lista.innerHTML = ''
+
+    jogadores.forEach((jogador, indice) => {
+
+        const jogadorDiv = document.createElement('div')
+
+        jogadorDiv.innerText = `
+            Índice: ${indice}
+            Nome: ${jogador.nome}
+            Posição: ${jogador.posicao}
+            Número: ${jogador.numero}
+        `
+
+        lista.appendChild(jogadorDiv)
+    })
 }
 
-remove.addEventListener('click', ()=>{
-    
+
+// REMOVER JOGADOR
+btnRemove.addEventListener('click', () => {
+
+    const indice = +prompt('Digite o índice do jogador que deseja remover')
+
+    jogadores.splice(indice, 1)
+
+    renderizarJogadores() 
+    /* const excluir = createElement('input')
+    excluir.placeholder = 'Excluir jogador pelo indice' */
 })
+
