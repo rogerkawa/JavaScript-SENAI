3b521bc7477a7e26d9fff9b5f40593de9ca5b137
Roger Kawã <rogerkawa8@gmail.com>
2026-05-14 14:32:49 -0300

    Add files via upload

--- EscalaçãoTimeDOM/app.js
+++ EscalaçãoTimeDOM/app.js
@@ -8,22 +8,32 @@ const position = document.createElement('input')
 const number = document.createElement('input')
 
 const btnAdd = document.createElement('button')
+
+const inputRemove = document.createElement('input')
+
 const btnRemove = document.createElement('button')
 
 const lista = document.createElement('div')
 
+lista.style.textAlign = 'center'
+lista.style.height = '60vh'
+lista.style.border = '1px solid black'
+lista.style.width = '15vw'
+
 nome.classList.add('input')
 position.classList.add('input')
 number.classList.add('input')
+inputRemove.classList.add('input')
 
 nome.placeholder = 'Digite o nome do jogador'
 position.placeholder = 'Digite a posição do jogador'
 number.placeholder = 'Digite o número do jogador'
+inputRemove.placeholder = 'Digite o indice do jogador que deseja remover'
 
 btnAdd.innerText = 'Adicionar Jogador'
 btnRemove.innerText = 'Remover Jogador'
 
-body.append(nome, position, number, btnAdd, btnRemove, lista)
+body.append(nome, position, number, btnAdd,inputRemove, btnRemove, lista)
 
 btnAdd.addEventListener('click', () => {
 
@@ -66,11 +76,13 @@ function renderizarJogadores() {
 // REMOVER JOGADOR
 btnRemove.addEventListener('click', () => {
 
-    const indice = +prompt('Digite o índice do jogador que deseja remover')
+    /* const indice = +prompt('Digite o índice do jogador que deseja remover') */
+    const indice = inputRemove.value
 
     jogadores.splice(indice, 1)
-
+    
     renderizarJogadores() 
+    inputRemove.value = ''
     /* const excluir = createElement('input')
     excluir.placeholder = 'Excluir jogador pelo indice' */
 })
